e667857cdec9a623901c9a17079bfbb442d84ecc
judtruji <judtruji@bancolombia.com.co>
2024-09-01 11:21:05 -0500

    Commit

--- Clase/src/main/java/org/example/tarea1/main.java
+++ Clase/src/main/java/org/example/tarea1/main.java
@@ -1,12 +1,13 @@
 package main.java.org.example.tarea1;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class main {
 
     public static void main(String[] args) {
-
 
         List<Persona> personas = Arrays.asList(
                 new Persona("Juan", 25, "Masculino"),
@@ -18,16 +19,31 @@ public class main {
         //Filter: Filtrar personas mayores de 25 años.
         System.out.println(personas.stream()
                 .filter(persona -> persona.getEdad() >= 25)
-                .toList());
+                .collect(Collectors.toList()));
 
         //Map: Obtener una lista con los nombres de las personas.
         System.out.println(personas.stream()
                 .map(Persona::getNombre)
-                .toList());
+                .collect(Collectors.toList()));
 
         //Reduce: Obtener la suma de las edades de todas las personas.
         System.out.println(personas.stream()
                 .map(Persona::getEdad)
-                .reduce(0,Integer::sum));
+                .reduce(0, Integer::sum));
+
+        //Ejercicios Extra
+
+        //Contar la cantidad de personas de cada género.
+        System.out.println(personas.stream()
+                .collect(Collectors.groupingBy(Persona::getGenero, Collectors.counting())));
+
+        //Calcular el promedio de edades de las personas.
+        System.out.println(personas.stream()
+                .mapToInt(Persona::getEdad)
+                .average());
+
+        //Encontrar la persona de mayor edad.
+        System.out.println(personas.stream()
+                .max(Comparator.comparingInt(Persona::getEdad)));
     }
 }
